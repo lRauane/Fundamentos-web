833fe7b18159117ab7995383ffad01bbbd9e0dd5
Rauane <rauanelima7@gmail.com>
2022-06-27 15:40:37 -0300

    nova funcionalidade

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -18,3 +18,20 @@ function toggleMenu(event){
 btnmobile.addEventListener('click', toggleMenu);
 btnmobile.addEventListener('touchstart', toggleMenu);
 /*menu */
+
+const btnEl = document.querySelector('.btn');
+
+const toggleOptions = () => {
+  const wrapperEl = document.querySelector('.wrapper');
+  const iconEl = btnEl.querySelector('i');
+
+  wrapperEl.classList.toggle('active');
+
+  if (iconEl.classList.contains('ri-share-line')) {
+    iconEl.classList.replace('ri-share-line', 'ri-close-line');
+  } else {
+    iconEl.classList.replace('ri-close-line', 'ri-share-line');
+  }
+};
+
+btnEl.addEventListener('click', toggleOptions);
